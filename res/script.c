4006904e2ee6e87c4c161f0e40c5cc23c26c2b3c
Kostia <60094563+byquip@users.noreply.github.com>
2022-05-28 10:29:56 +0200

    Update script.c

--- res/script.c
+++ res/script.c
@@ -15,27 +15,29 @@ EXPORT int init(const char *str) {
 	return 1;
 }
 
-EXPORT const char *getLastError() {
+EXPORT const char *getLastError() { 
 	return error;
 }
 
 EXPORT int eval(const char *func,
-	int nArgs,
-	const double **inReal,
-	const double **inImag,
-	int blockSize,
-	double *outReal,
-	double *outImag) {
-	int i, j;
-	if (strcmp("test_func", func) == 0) {
-		if (nArgs != 2) {
+	int nArgs,              // <--- Number of arguments given
+	const double **inReal,  // <--- Matrix of real part of arguments
+	const double **inImag,  // <--- Matrix of imag part of arguments
+	int blockSize,          // <--- Length of columns with arguments
+	double *outReal,        // <--- Column of real part of output
+	double *outImag) {      // <--- Column of imag part of output
+	int i;
+	if (strcmp("test_func", func) == 0) { // <--- Check if we call right function from dll
+		if (nArgs != 2) {             // <--- Check if we use right number of arguments
 			error = "Two argument expected";
 			return 0;
 		}
-		for (i = 0; i < blockSize; i++) {
-			double a = inReal[0][i];
-			double b = inReal[1][i];
+		for (i = 0; i < blockSize; i++) { // <--- looping over every value in columns
+                        // -------------------------------->MY CODE HERE<--------------------------------
+			double a = inReal[0][i];  // <--- take real part of first ([0]) argument.
+			double b = inReal[1][i];  // <--- take real part of second ([1]) argument.
 			outReal[i] = a^2+b^2;
+                        // -------------------------------->MY CODE HERE<--------------------------------
 		}
 
 		return 1;
